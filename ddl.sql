30a116841bf0e7ecae31644aaee1220f1fe7c4b6
HansHuang <42678103+Huanxin1997@users.noreply.github.com>
2020-07-15 11:22:23 +0800

    【Update】Revise the table's storage engine

--- ddl.sql
+++ ddl.sql
@@ -1,37 +1,50 @@
 -- **Database level:**
 -- Display all the database
 SHOW DATABASE;
+
 -- Enter a certain database
 USE <DATABASE>;
+
 -- Create a database
 CREATE DATABASE dbname;
+
 -- Create the database of the designated character set
 CREATE DATABASE `new_schema` DEFAULT CHARACTER SET utf8 ;
+
 -- Display the creation information fo the database
 SHOW CREATE DATABASE sys;
+
 -- Revise the codes of the database
 
 -- Delete a database
 DROP DATABASE `sys`;
+
 -- **Table level**
 -- Revise table name
 ALTER TABLE `student_examination_sys`.`score` 
 RENAME TO  `student_examination_sys`.`score1` ;
+
 -- Revise the field's data type
 ALTER TABLE `student_examination_sys`.`student` 
-CHANGE COLUMN `sex` `sex` INT NOT NULL ;
+CHANGE COLUMN `sex` `sex` INT NOT NULL;
+
 -- Revise field name
 ALTER TABLE `student_examination_sys`.`student` 
-CHANGE COLUMN `sex` `sex1` VARCHAR(45) NOT NULL ;
+CHANGE COLUMN `sex` `sex1` VARCHAR(45) NOT NULL;
+
 -- Add field
 ALTER TABLE `student_examination_sys`.`student` 
 ADD COLUMN `studentcol` VARCHAR(45) NULL AFTER `sex`;
+
 -- Delete field
 ALTER TABLE `student_examination_sys`.`student` 
 DROP COLUMN `studentcol`;
+
 -- Revise the table's storage engine
+ALTER TABLE `student` engine=innodb;
 
 -- Delete the table's foreign key restriant
+ALTER TABLE `student` DROP foreign key foreignKeyName;
 
 -- Delete a table
 DROP TABLE `student_examination_sys`.`new_table`;
